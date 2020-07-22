ef6b7993f227312345051922c12706f1bcc3cac7
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-07-22 14:01:10 +0900

    Router add

--- ec-app/src/reducks/store/store.js
+++ ec-app/src/reducks/store/store.js
@@ -1,10 +1,17 @@
-import { createStore as reduxCreateStore, combineReducers } from "redux";
+import {
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import { UserReducer } from "../users/reducer";
 
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
+      router: connectRouter(history),
       users: UserReducer,
-    })
+    }),
+    applyMiddleware(routerMiddleware(history))
   );
 }
