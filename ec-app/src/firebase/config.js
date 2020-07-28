2fe130a0332254a2708c20514dcd820c7210e6e9
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-07-28 21:44:42 +0900

    firebase add

---
+++ ec-app/src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBA-5F_4tR2F98-ckMT7NDOicCk5yIaXGU",
+    authDomain: "ec-application-2a7f2.firebaseapp.com",
+    databaseURL: "https://ec-application-2a7f2.firebaseio.com",
+    projectId: "ec-application-2a7f2",
+    storageBucket: "ec-application-2a7f2.appspot.com",
+    messagingSenderId: "1030006492919",
+    appId: "1:1030006492919:web:b1024a215566d2d2094df2",
+    measurementId: "G-F3XZ9VCE50"
+  };
